1879b251d0d57e3e2b1610db36336d2e7a8230e7
Taiwo Ibrahim <ibrahimtaiwoti@gmail.com>
2024-11-25 22:31:03 +0100

    message

--- src/Components/CartItem/CartItem.jsx
+++ src/Components/CartItem/CartItem.jsx
@@ -8,6 +8,7 @@ function CartItem() {
         <div className="cartItem__container-q1">
           <Image width={70} height={70} src="/product1.png" alt="" />
           <div className="cartItem__container-q1_text">
+            <p className="responsive__topic">PRODUCT:</p>
             <h1>Summer Round Neck Tee ‘25 - Black </h1>
             <p>
               Color: <span>Black</span>
@@ -15,13 +16,48 @@ function CartItem() {
           </div>
         </div>
         <div className="cartItem__container-q2">
+          <p className="responsive__topic">PRODUCT:</p>
           <p>N100,000</p>
         </div>
         <div className="cartItem__container-q2">
+          <p className="responsive__topic">PRODUCT:</p>
           <p>1</p>
         </div>
         <div className="cartItem__container-q2">
+          <p className="responsive__topic">PRODUCT:</p>
           <p>N100,000</p>
+        </div>
+
+
+        <div className="cart__responsive">
+          <div className="cartItem__container-q1_responsive">
+            <div className="cartItem__container-q1_img">
+              <Image width={70} height={70} src="/product1.png" alt="" />
+              <img src="/close.png" alt=" " />
+
+            </div>
+            <div className="cartItem__container-q1_text-responsive">
+              <p className="responsive__topic">PRODUCT:</p>
+              <div className="cartItem__container-q1_text-responsive-text">
+                <h1>Summer Round Neck Tee ‘25 - Black </h1>
+                {/* <p>
+                  Color: <span>Black</span>
+                </p> */}
+              </div>
+            </div>
+          </div>
+          <div className="cartItem__container-q2_responsive">
+            <p className="responsive__topic">PRICE:</p>
+            <p>N100,000</p>
+          </div>
+          <div className="cartItem__container-q2_responsive">
+            <p className="responsive__topic">QUANTITY:</p>
+            <p>1</p>
+          </div>
+          <div className="cartItem__container-q2_responsive">
+            <p className="responsive__topic">SUBTOTAL:</p>
+            <p>N100,000</p>
+          </div>
         </div>
       </div>
     )
